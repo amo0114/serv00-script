4a5efae3c384e5eb90037480707f5dbbd0783987
amo0114 <108148125+amo0114@users.noreply.github.com>
2024-08-06 15:01:58 +0800

    Update recover_vless.py
    分段发送

--- vless/recover_vless.py
+++ vless/recover_vless.py
@@ -5,20 +5,23 @@ import requests
 
 def send_telegram_message(token, chat_id, message):
     telegram_url = f"https://api.telegram.org/bot{token}/sendMessage"
-    telegram_payload = {
-        "chat_id": chat_id,
-        "text": message,
-        "reply_markup": '{"inline_keyboard":[[{"text":"问题反馈❓","url":"https://t.me/yxjsjl"}]]}'
-    }
+    max_length = 4096  # Telegram API 规定消息长度不能超过4096个字符
 
-    response = requests.post(telegram_url, json=telegram_payload)
-    print(f"Telegram 请求状态码：{response.status_code}")
-    print(f"Telegram 请求返回内容：{response.text}")
+    # 如果消息超过最大长度，分段发送
+    for i in range(0, len(message), max_length):
+        telegram_payload = {
+            "chat_id": chat_id,
+            "text": message[i:i+max_length],
+            "reply_markup": '{"inline_keyboard":[[{"text":"问题反馈❓","url":"https://t.me/yxjsjl"}]]}'
+        }
+        response = requests.post(telegram_url, json=telegram_payload)
+        print(f"Telegram 请求状态码：{response.status_code}")
+        print(f"Telegram 请求返回内容：{response.text}")
 
-    if response.status_code != 200:
-        print("发送 Telegram 消息失败")
-    else:
-        print("发送 Telegram 消息成功")
+        if response.status_code != 200:
+            print("发送 Telegram 消息失败")
+        else:
+            print("发送 Telegram 消息成功")
 
 # 从环境变量中获取密钥
 accounts_json = os.getenv('ACCOUNTS_JSON')
